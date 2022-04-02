af3fff466fdd41febba8dcc6a531be6a8210b1fa
yusuf <yusufani8@gmail.com>
2022-04-02 23:21:10 +0000

    Bump version: 0.1.16 → 0.1.17

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/yusufani/trclip',
-    version='0.1.16',
+    version='0.1.17',
     zip_safe=False,
 )
